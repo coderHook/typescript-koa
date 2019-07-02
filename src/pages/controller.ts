2099629a31ce280282c99e32a97ca51f99641f26
coderhook <abel1987@hotmail.es>
2019-07-02 12:12:19 +0200

    Added allPages

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,8 +1,15 @@
 import { JsonController, Get, Param } from 'routing-controllers'
-import pagesById, { Page } from './data'
+import { pagesById, pages, Page } from './data'
+
+type PageList = { pages: Page[] }
 
 @JsonController()
 export default class PageController {
+  @Get('/pages/')
+  allPages(): PageList {
+    return pages;
+  }
+  
   @Get('/pages/:id')
   getPage(
     @Param('id') id: number
--- src/pages/data.ts
+++ src/pages/data.ts
@@ -4,11 +4,39 @@ export interface Page {
   content: string
 }
 
+
 interface PageDatabase {
   [id: number]: Page
 }
 
-const pagesById: PageDatabase = {
+// interface PagesDatabase {
+//   [index: number]: { 
+//       id: number; 
+//       content: string; 
+//       title: string };
+// }
+
+export const pages = {
+  "pages": [
+      {
+          "content": "<h1>Homepage</h1>This is the homepage, and everything starts with a <strong>home</strong>page.",
+          "id": 1,
+          "title": "Homepage"
+      },
+      {
+          "content": "<h1>Links</h1>Here's where we will give you <i>links</i> to other pages.",
+          "id": 2,
+          "title": "Links"
+      },
+      {
+          "content": "<h1>Social media</h1>This is where you'll find links to our social media thing. Find us on Insta!",
+          "id": 3,
+          "title": "Social media"
+      }
+  ]
+}
+
+export const pagesById: PageDatabase = {
   1: {
     id: 1,
     title: 'Homepage',
@@ -29,4 +57,3 @@ const pagesById: PageDatabase = {
   }
 }
 
-export default pagesById
